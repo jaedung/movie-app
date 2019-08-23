518c5d8c6ab9e040f7f398f33b3b472a14716983
justin <jkim@wolas.com.au>
2019-08-23 11:13:53 +1000

    Updates

---
+++ precache-manifest.89d44d3d09bc26918f8ef8ed142908db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbeea6e3d32455f6163282bd84ea96be",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "ab522a493902466fdb8f",
+    "url": "/movie-app/static/css/main.7cad1c89.chunk.css"
+  },
+  {
+    "revision": "5d82e887cbff2beb0f9a",
+    "url": "/movie-app/static/js/2.da9c519f.chunk.js"
+  },
+  {
+    "revision": "ab522a493902466fdb8f",
+    "url": "/movie-app/static/js/main.70a03549.chunk.js"
+  },
+  {
+    "revision": "d8c18da1c3f645f35d82",
+    "url": "/movie-app/static/js/runtime~main.1593d493.js"
+  }
+]);
